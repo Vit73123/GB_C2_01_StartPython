76c23674e020bf9609086b61aa2b0908cde4c890
vit73125 <vit73125@gmail.com>
2022-11-23 19:34:25 +0300

    L01 HW 00

---
+++ Seminars/l01htd01.py
@@ -0,0 +1,11 @@
+# (!!!Доп!!!)
+# Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            x = bool(x)
+            y = bool(y)
+            z = bool(z)
+            print(f"¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} = {not((x or y or z) == (not x and not y and not z))}")
