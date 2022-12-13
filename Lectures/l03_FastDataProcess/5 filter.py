5a595c65a52cfcc045144c0ad9dd89cc0183eef0
vit73125 <vit73125@gmail.com>
2022-12-13 16:51:42 +0300

    fix: L02, L03 Lectures

--- Lectures/l03_FastDataProcess/5 filter.py
+++ Lectures/l03_FastDataProcess/5 filter.py
@@ -10,8 +10,10 @@ def where(f, col):
 data = '1 2 3 4 5 8 15 23 38'.split()
 # data = list(map(int, '1 2 3 4 555 6'.split()))  # map с преобразованием в list
 
+# res = select(int, data)
 res = map(int, data)
 res = where(lambda x: not x % 2, res)               # lambda функция для критерия отбора входного списка res в список только с чётными числами
+# res = list(select(lambda x: (x, x ** 2), res))            # lambda функция для построения списка пар (x, x^2), где x - чётные
 res = list(map(lambda x: (x, x ** 2), res))            # lambda функция для построения списка пар (x, x^2), где x - чётные
 print(res)
 
