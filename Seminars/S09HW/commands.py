580ee46de328ce5c33202a8836080d7372d865eb
vit73125 <vit73125@gmail.com>
2022-12-22 17:32:40 +0300

    fix: S09 HW

--- Seminars/S09HW/commands.py
+++ Seminars/S09HW/commands.py
@@ -3,5 +3,4 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def msg_filter(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
-    result = ' '.join([word for word in update.message.text.split() if not 'абв' in word])
-    await update.message.reply_text(result)
+    await update.message.reply_text(' '.join([word for word in update.message.text.split() if not 'абв' in word]))
